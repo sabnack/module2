37fba33be8db9294cb0bf705c6d2de94b2d4a2c6
tojarr <30999615+tojarr@users.noreply.github.com>
2018-12-10 20:29:36 +0200

    Create class Feature

---
+++ BugTracking/BugTracking/Feature.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracking
+{
+    class Feature:Task
+    {
+        public const int Priority = 1;
+    }
+}
