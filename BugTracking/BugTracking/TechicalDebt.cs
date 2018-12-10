219074dccb748ac984c2af01e7ffbb9b03d9ccc8
tojarr <30999615+tojarr@users.noreply.github.com>
2018-12-10 21:13:59 +0200

    Update TechicalDebt.cs

--- BugTracking/BugTracking/TechicalDebt.cs
+++ BugTracking/BugTracking/TechicalDebt.cs
@@ -9,5 +9,10 @@ namespace BugTracking
     class TechicalDebt:Task
     {
         public const double Priority = 0.5;
+
+        public TechicalDebt(int number, string description, int complexity)
+            : base( number, description, complexity)
+        {
+        }
     }
 }
