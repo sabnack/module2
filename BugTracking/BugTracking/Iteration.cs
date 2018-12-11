ac213252a23ceef1752fef880ab39a6292e82f20
Станислав Горожанкин <sgorozhankin@gmail.com>
2018-12-11 15:38:17 +0200

    Add logic

--- BugTracking/BugTracking/Iteration.cs
+++ BugTracking/BugTracking/Iteration.cs
@@ -18,5 +18,81 @@ namespace BugTracking
         {
             _tasksList = _taskListInit.GetTasksList();
         }
+
+        public void StartProgram()
+        {
+            for (int i = 0; i < _namberIteration; i++)
+            {
+                CreateTemporaryList();
+
+                if (_temporaryList.Count == 0)
+                {
+                    Console.WriteLine("All task Done");
+                    break;
+                }
+
+                Console.WriteLine("Iteration: {0} ", (i + 1));
+                StartIteration();
+                Console.WriteLine();
+
+                if (i == _namberIteration - 1)
+                {
+                    Console.WriteLine("Iteration end");
+                }
+            }
+        }
+
+        public void CreateTemporaryList()
+        {
+            _temporaryList.Clear();
+            for (int i = 0; i < _tasksList.Count; i++)
+            {
+                if (_tasksList[i].Status == "ToDo")
+                {
+                    _temporaryList.Add(_tasksList[i]);
+                    _tasksList[i].Status = "InProgress";
+                    if (_temporaryList.Count == _namberOfIterationItems)
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void StartIteration()
+        {
+            foreach (var item in _temporaryList)
+            {
+                for (int i = 0; i < item.Complexity * item.Priority; i++)
+                {
+                    if (item is Feature)
+                    {
+                        if (new Random().Next(6) > 3)
+                        {
+                            item.Status = "Bug";
+
+                            _tasksList.Add(new Bug("Bug" + (Task.Number + 1), new Random().Next(1, 4), item));
+                            Console.WriteLine("Create Bug.");
+                            break;
+                        }
+                    }
+                    Console.WriteLine("ID:{0} - {1} - Iteration: {2}", item.Id, item.Description, (i + 1));
+                }
+
+                if (item.Status == "Bug")
+                {
+                    continue;
+                }
+
+                if (item is Bug item1)
+                {
+                    if (item1.Feature != null)
+                    {
+                        item1.Feature.Status = "Done";
+                    }
+                }
+                item.Status = "Done";
+            }
+        }
     }
 }
